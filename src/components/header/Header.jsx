40613fc22eb9773106e827383abc000242663669
Aman Gupta <amangupta99344@gmail.com>
2023-02-20 00:22:18 +0530

    simple Resume react components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Aman Gupta</h1>
+        <p>Software Developer, Educator at Geekster</p>
+        <a href="#">Linkedin  </a>
+        <a href="#">Github</a>
+        <hr />
+    </div>
+  )
+}
+
+export default Header;
